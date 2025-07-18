2bc0f72685768b5af97b7ec51ba9786d903c171d
Denis Nabokov <denis.nabokov@eit.lth.se>
2025-07-18 15:07:26 +0200

    debug prints

--- kyber512_test.py
+++ kyber512_test.py
@@ -12,11 +12,11 @@ oracle = KyberOracle("127.0.0.1", 3334)
 
 while True:
     guess = recv_exact(oracle._sock, 1)
-    # print(f"{guess=}, len={len(guess)}")
+    print(f"{guess=}, len={len(guess)}")
     block_idx = recv_exact(oracle._sock, 1)
-    # print(f"{block_idx=}, len={len(block_idx)}")
+    print(f"{block_idx=}, len={len(block_idx)}")
     flip_idx = recv_exact(oracle._sock, 1)
-    # print(f"{flip_idx=}, len={len(flip_idx)}")
+    print(f"{flip_idx=}, len={len(flip_idx)}")
     assert len(guess) == 1
     assert len(block_idx) == 1
     assert len(flip_idx) == 1
