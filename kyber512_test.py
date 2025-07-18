a168bbd3bf209c2a07893eb859fc80baff9d756e
Denis Nabokov <denis.nabokov@eit.lth.se>
2025-07-18 14:40:33 +0200

    debug prints

--- kyber512_test.py
+++ kyber512_test.py
@@ -12,10 +12,10 @@ oracle = KyberOracle("127.0.0.1", 3334)
 
 while True:
     guess = recv_exact(oracle._sock, 1)
-    block_idx = recv_exact(oracle._sock, 1)
-    flip_idx = recv_exact(oracle._sock, 1)
     print(f"{guess=}, len={len(guess)}")
+    block_idx = recv_exact(oracle._sock, 1)
     print(f"{block_idx=}, len={len(block_idx)}")
+    flip_idx = recv_exact(oracle._sock, 1)
     print(f"{flip_idx=}, len={len(flip_idx)}")
     oracle.lowest_message_bit = flip_idx
     ct = build_naive_ciphertext(
