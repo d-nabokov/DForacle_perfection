838b826eaca0845da4cb6aa418f0cc3c484769a0
Denis Nabokov <denis.nabokov@eit.lth.se>
2025-07-18 16:48:06 +0200

    sk diff

---
+++ accuracy_analysis/check_sk_correct.py
@@ -0,0 +1,37 @@
+import re
+from pathlib import Path
+
+
+def load_secret_key(path):
+    """Read 512 integers from kyber.txt."""
+    with path.open() as f:
+        sk = [int(line) for line in f if line.strip()]
+    if len(sk) != 512:
+        raise ValueError(f"{path} should contain 512 lines, found {len(sk)}")
+    return sk
+
+
+def load_guess_key(path):
+    """Read 512 integers from kyber.txt."""
+    p = re.compile("bit:(\d+),guess:(-?\d+)")
+    guesses = {}
+    with path.open() as f:
+        for line in f:
+            m = p.match(line.strip())
+            if m:
+                guesses[int(m[1])] = int(m[2])
+    return guesses
+
+
+path = Path("../GoFetch/poc/crypto_victim/kyber.txt")
+sk = load_secret_key(path)
+# print(sk)
+
+path_guess = Path("../GoFetch/poc/crypto_attacker/kyber.txt")
+sk_guess = load_guess_key(path_guess)
+# print(sk_guess)
+
+for idx, val in sk_guess.items():
+    if sk[idx] != val:
+        print(f"{idx}: Expect={sk[idx]}, got {val}")
+print("Done")
