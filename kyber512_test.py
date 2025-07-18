70b027707bd56ef06b76238f5005f4c7144a02ee
Denis Nabokov <denis.nabokov@eit.lth.se>
2025-07-18 14:59:44 +0200

    convert to int

--- kyber512_test.py
+++ kyber512_test.py
@@ -12,11 +12,17 @@ oracle = KyberOracle("127.0.0.1", 3334)
 
 while True:
     guess = recv_exact(oracle._sock, 1)
-    print(f"{guess=}, len={len(guess)}")
+    # print(f"{guess=}, len={len(guess)}")
     block_idx = recv_exact(oracle._sock, 1)
-    print(f"{block_idx=}, len={len(block_idx)}")
+    # print(f"{block_idx=}, len={len(block_idx)}")
     flip_idx = recv_exact(oracle._sock, 1)
-    print(f"{flip_idx=}, len={len(flip_idx)}")
+    # print(f"{flip_idx=}, len={len(flip_idx)}")
+    assert len(guess) == 1
+    assert len(block_idx) == 1
+    assert len(flip_idx) == 1
+    guess = int(guess[0])
+    block_idx = int(block_idx[0])
+    flip_idx = int(flip_idx[0])
     oracle.lowest_message_bit = flip_idx
     ct = build_naive_ciphertext(
         z_value=208,
